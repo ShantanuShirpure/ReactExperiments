f9ef78ea226129ffcaf69b2c911c1d86aa64d733
ShantanuShirpure <shantanu.shirpure@live.com>
2018-01-07 23:35:31 -0500

    videdetail

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY ='AIzaSyD8w3pRgJXkZzq2HPFFYHg07vd2dx_-i-8';
 
-YTSearch({key :API_KEY, term:'Shantanu Shirpure'}, 
+YTSearch({key :API_KEY, term:'Surfboard'}, 
 (data) =>{
     console.log(data);
 }
@@ -16,7 +17,7 @@ class App extends Component {
         super(props);
         this.state={videos:[]};
 
-        YTSearch({key :API_KEY, term:'Shantanu Shirpure'}, 
+        YTSearch({key :API_KEY, term:'Surfboard'}, 
         (videos) =>{
                 this.setState({videos});
         }
@@ -26,6 +27,7 @@ class App extends Component {
          return (
         <div>
          <SearchBar />
+         <VideoDetail video = {this.state.videos[0]} />
          <VideoList videos = {this.state.videos} />
         </div>
     );
